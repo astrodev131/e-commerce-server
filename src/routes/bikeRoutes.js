b099444e4f157ad7480cc541539afe9569d165fe
astrodev131 <astro.dev131@gmail.com>
2024-12-18 13:02:13 -0600

    finish

--- src/routes/bikeRoutes.js
+++ src/routes/bikeRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { addBike } = require("../controllers/bikeController");
+const { addBike, getBike } = require("../controllers/bikeController");
 const router = express.Router();
 
 // Register route
 
 // Login route
 router.post("/add", addBike);
+router.get("/getdata", getBike);
 
 module.exports = router;
